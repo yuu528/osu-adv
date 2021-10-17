562b8cb76852e637ced86c0e74c49ba4c0eb36b1
Yuu528 <yuu528mail@gmail.com>
2021-10-17 21:39:25 +0900

    Version 0.3.1

--- osuadv.user.js
+++ osuadv.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         osu!adv
 // @namespace    https://yuu-g.net/
-// @version      0.3
+// @version      0.3.1
 // @description  Add new features to osu! website.
 // @author       Yuu528
 // @match        https://osu.ppy.sh/*
